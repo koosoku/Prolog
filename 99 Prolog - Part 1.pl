00046ae28298beaf121e34069e299b45bc164a63
Kyle <kylekung@hotmail.com>
2016-02-12 18:41:39 -0500

    Problem 3 Find the Kth Element of the list

--- 99 Prolog - Part 1.pl
+++ 99 Prolog - Part 1.pl
@@ -8,3 +8,11 @@ findLast([_|T],X):-
 findSecondLast([H,_],H):-!.
 findSecondLast([_|T],X):- 
 	findSecondLast(T,X).
+%?- findSecondLast([1,2,3,4,5,a],X).
+
+%Find the kth element of a list.
+findKth(1,[H|_],H):-!.
+findKth(Count,[_|T],X):-
+	CountOneLess is Count - 1,
+	findKth(CountOneLess,T,X).
+%?- findKth(3,[1,2,3,4,5],X).
